a1b4f213c48b179052b106df47cd80e81efd732d
antoine-agre <antoine.agre@free.fr>
2022-03-13 22:45:34 +0100

    Ajoute attribut duree à Trajet
    Ajoute attribut entier duree, en minutes.

--- src/donnees/Trajet.java
+++ src/donnees/Trajet.java
@@ -11,11 +11,13 @@ public class Trajet {
     protected Station arrivee;
     protected TreeSet<Date> listeHoraires = new TreeSet<>();
     protected MoyenTransport moyenTransport;
+    protected int duree; //durée du trajet en minutess
 
-    public Trajet(Station depart, Station arrivee, MoyenTransport moyenTransport){
+    public Trajet(Station depart, Station arrivee, MoyenTransport moyenTransport, int duree){
         this.depart = depart;
         this.arrivee = arrivee;
         this.moyenTransport = moyenTransport;
+        this.duree = duree;
     }
 
 
