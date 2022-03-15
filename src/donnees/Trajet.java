d1bfe414f6c78957b8cf822b92a39f56ea29fb7a
antoine-agre <antoine.agre@free.fr>
2022-03-15 16:41:41 +0100

    Renomme attribut Duree en duree

--- src/donnees/Trajet.java
+++ src/donnees/Trajet.java
@@ -1,8 +1,6 @@
 package donnees;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class Trajet {
@@ -11,13 +9,13 @@ public class Trajet {
     public Station arrivee;
     protected TreeSet<Date> listeHoraires = new TreeSet<>();
     protected MoyenTransport moyenTransport;
-    public int Duree;
+    public int duree;
     //ajout de la variable de temps 
     //songer a un convertisseur en heure apres lajout de la duree sur le temps de depart
     public Trajet(Station depart, Station arrivee, MoyenTransport moyenTransport, int Duree){
         this.depart = depart;
         this.arrivee = arrivee;
-        this.Duree=Duree;
+        this.duree =Duree;
         this.moyenTransport = moyenTransport;
     }
 
